625d8fb70d5e44f88113f78ad374de72399b0c55
EC2 Default User <ec2-user@ip-10-0-0-165.ap-northeast-1.compute.internal>
2020-10-28 12:03:29 +0000

    inital HelloWorld Project

---
+++ src/main/java/com/devopslab/HelloWorldController.java
@@ -0,0 +1,27 @@
+package com.devopslab;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@RequestMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public String helloWorld(String name) {
+		try {
+			System.out.print("Got hello request from "+name);
+			String localip=InetAddress.getLocalHost().getHostAddress();
+			return "Hello, "+name+"!, you are from: "+localip;
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "Hello, "+name+"!";
+		
+	}
+	
+}
