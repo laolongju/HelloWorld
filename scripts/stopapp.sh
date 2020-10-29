0802d405280117881cabcf41102369693a1b9dda
EC2 Default User <ec2-user@ip-10-0-0-165.ap-northeast-1.compute.internal>
2020-10-29 02:12:40 +0000

    add stop app scripts

---
+++ scripts/stopapp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PID=$(ps -ef | grep HelloWorldService-1.0-SNAPSHOT.jar | grep -v grep | awk '{ print $2 }')
+if [ ${PID} ]; 
+then
+	 echo 'Application is stpping...'
+	  echo kill $PID DONE
+	   kill $PID
+   else
+	    echo 'Application is already stopped...'
+fi
