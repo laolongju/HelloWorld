7c27d69e252e9721e7cda821bbb587115940a96d
laolongju <jxl@186590ce5fed.ant.amazon.com>
2020-11-13 20:39:22 +0800

    update

--- src/main/java/com/devopslab/HelloWorldController.java
+++ src/main/java/com/devopslab/HelloWorldController.java
@@ -3,14 +3,13 @@ package com.devopslab;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 	
-	@RequestMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@RequestMapping(value = "/hello")
 	public String helloWorld(String name) {
 		try {
 			System.out.print("Got hello request from "+name);
@@ -24,7 +23,7 @@ public class HelloWorldController {
 		
 	}
 
-	@RequestMapping(value = "/echo", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@RequestMapping(value = "/echo")
 	public String echo(String name) {
 		try {
 			System.out.print("Got hello request from "+name);
