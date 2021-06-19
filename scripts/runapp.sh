e5e36721273ea5c6641346452b253ee42dbafebf
jxl <jxl@192.168.0.108>
2021-06-19 19:23:27 +0800

    update script for jenkins deploy

--- scripts/runapp.sh
+++ scripts/runapp.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo 'Starting Spring Boot app'
 cd ~/hello
-nohup java -jar HelloWorldService-1.0-SNAPSHOT.jar >/dev/null 2>&1 &
+nohup java -jar target/HelloWorldService-1.0-SNAPSHOT.jar >/dev/null 2>&1 &
--- src/main/java/com/devopslab/HelloWorldController.java
+++ src/main/java/com/devopslab/HelloWorldController.java
@@ -3,16 +3,20 @@ package com.devopslab;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class HelloWorldController {
-	
-	@RequestMapping(value = "/hello")
+	 private static final Log log = LogFactory.getLog(HelloWorldController.class);
+	@RequestMapping(value = "/api/hello")
 	public String helloWorld(String name) {
 		try {
-			System.out.print("Got hello request from "+name);
+			log.info("Got hello request from "+name);
+			log.debug("Debug get hello request from "+name);
 			String localip=InetAddress.getLocalHost().getHostAddress();
 			return "Hello, "+name+"!, you are visiting "+localip+" V10 version";
 		} catch (UnknownHostException e) {
@@ -22,11 +26,23 @@ public class HelloWorldController {
 		return "Hello, "+name+"!";
 		
 	}
-
+	@RequestMapping(value = "/")
+	public String defaultEcho() {
+		try {
+			log.info("Got hello request from default");
+			String localip=InetAddress.getLocalHost().getHostAddress();
+			return "Hello, default!, you are visiting "+localip+" V10 version";
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "Hello,default !";
+		
+	}
 	@RequestMapping(value = "/echo")
 	public String echo(String name) {
 		try {
-			System.out.print("Got hello request from "+name);
+			log.info("Got hello request from "+name);
 			String localip=InetAddress.getLocalHost().getHostAddress();
 			return "echo, "+name+"!, you are visiting "+localip+" V10 version";
 		} catch (UnknownHostException e) {
